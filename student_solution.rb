9ea32f8b4544070f14b343721b5f9808d7b3ef30
Kyle Thousand <kylethousand@kyles-mbp-2.lan>
2020-11-17 21:19:10 -0500

    colors array finished

--- student_solution.rb
+++ student_solution.rb
@@ -16,62 +16,66 @@ nums = [5, 5, 6, 7, 2, 4, 3, 2, 1, 9, 7, 6, 0, 0, 35, 19, 66, 3, 22, 21, 4, 4, 4
 puts nums.uniq
 
 #2
-
-
+nums.push(5)
+nums.pop()
+nums.shift()
+nums.unshift()
+nums.length()
 #3
-
+puts nums.include?(8)
 
 #4
-
+puts nums.find_all {|i| i>10}
 
 #5
-
+puts nums.all? {|i| i>0}
 
 #6
-
+puts nums.any?{|i| i % 8 == 0}
 
 #7
-
+puts nums.count{|i| i>4}
 
 #8
-
+nums.each_with_index {|i, index| puts i * index}
 
 #9
-
+puts nums.find{|i| i % 5 == 0 && i % 7 == 0 && i > 0 }
 
 #10
+puts nums.find_index{|i| i % 5 == 0 && i % 7 == 0 && i > 0 }
 
 
 #11
-
+puts nums.first(3)
 
 #12
-
+puts nums.last(3)
 
 #13
-
+puts nums.group_by{|i| i % 3}
 
 #14
-
+puts nums.minmax{|i, j| i<=> j}
 
 #15
-
+puts nums.reject{|i| i % 3 != 0}
 
 #16
+puts nums.select{|i|i % 5 == 0}
 
-
-
+puts "----------------------------------------"
 
 # Color Array
-
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 #1
-
+puts colors.sample
 
 #2
-
+puts colors.reverse()
 
 #3
-
+puts colors.join(" ").upcase
 
 # Methods
 
