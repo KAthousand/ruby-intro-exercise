f135a297b22b4cc8599c9461f85232c6f2e286d6
Kyle Thousand <kylethousand@kyles-mbp-2.lan>
2020-11-17 21:01:21 -0500

    kyle thousand

--- student_solution.rb
+++ student_solution.rb
@@ -1,19 +1,19 @@
 # Hello World
 
 #1
-
+puts "Hello World"
 
 #2
-
-
+adjective = "Big Bad"
+puts "Hello #{adjective} World"
 #3
-
-
+message = "Hello World"
+puts message.upcase
 
 # Nums Array and Enumerables
-
+nums = [5, 5, 6, 7, 2, 4, 3, 2, 1, 9, 7, 6, 0, 0, 35, 19, 66, 3, 22, 21, 4, 4, 4, 0, 1, 2, 3, 4, 12, 15]
 #1
-
+puts nums.uniq
 
 #2
 
